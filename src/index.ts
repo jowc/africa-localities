8a9fa0148e3cf7e21f5266abb211f7c2ce0c761b
Joseph Chikeme <jchikeme@gmail.com>
2024-12-23 21:35:05 +0100

    updated project to ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-// src/index.js
-const express = require("express")
-const dotenv = require("dotenv")
+import express, {Express, Request, Response} from "express"
+import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT ?? 3000
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server")
 })
 
