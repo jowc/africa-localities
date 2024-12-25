f2e1a0903a2d3ce74e410ae69f981b71cd93089d
Joseph O. Chikeme <43372903+jowc@users.noreply.github.com>
2024-12-25 11:08:55 +0100

    sort localities alphabetically (#6)

--- src/data-access/services/localities.service.ts
+++ src/data-access/services/localities.service.ts
@@ -1,11 +1,30 @@
 import {FetchedSuccessfully} from "@utils/reponse-text.utils"
-import {LocalityRegionEnum} from "../models/localities.model"
+import {
+  LocalitiesInterface,
+  LocalityRegionEnum,
+} from "../models/localities.model"
 import {localitiesStore} from "../store/localities.store"
 import {ServerResponse} from "@utils/localities.utils"
 
-export const getAllLocalities = async () => {
-  const localities = await localitiesStore
-  return ServerResponse(200, localities, FetchedSuccessfully)
+export const getAllLocalities = () => {
+  const sortByName = (a: LocalitiesInterface, b: LocalitiesInterface) =>
+    a.name.localeCompare(b.name)
+
+  const sortLocalities = (
+    localities: LocalitiesInterface[]
+  ): LocalitiesInterface[] =>
+    localities.map((locality) => ({
+      ...locality,
+      children: locality.children
+        ? sortLocalities(locality.children.sort(sortByName))
+        : [],
+    }))
+
+  const sortedLocalities: LocalitiesInterface[] = sortLocalities(
+    localitiesStore.sort(sortByName)
+  )
+
+  return ServerResponse(200, sortedLocalities, FetchedSuccessfully)
 }
 
 export const getLocality = async (name: string, region: LocalityRegionEnum) => {
