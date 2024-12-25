25b8e35261bd71fb45a204f31d44cb69bb2207bf
Joseph Chikeme <jchikeme@gmail.com>
2024-12-25 11:36:31 +0100

    added prettier and linting

--- src/data-access/services/localities.service.ts
+++ src/data-access/services/localities.service.ts
@@ -8,27 +8,27 @@ import {ServerResponse} from "@utils/localities.utils"
 
 export const getAllLocalities = () => {
   const sortByName = (a: LocalitiesInterface, b: LocalitiesInterface) =>
-    a.name.localeCompare(b.name)
+    a.name.localeCompare(b.name);
 
   const sortLocalities = (
-    localities: LocalitiesInterface[]
+    localities: LocalitiesInterface[],
   ): LocalitiesInterface[] =>
     localities.map((locality) => ({
       ...locality,
       children: locality.children
         ? sortLocalities(locality.children.sort(sortByName))
         : [],
-    }))
+    }));
 
   const sortedLocalities: LocalitiesInterface[] = sortLocalities(
-    localitiesStore.sort(sortByName)
-  )
+    localitiesStore.sort(sortByName),
+  );
 
-  return ServerResponse(200, sortedLocalities, FetchedSuccessfully)
-}
+  return ServerResponse(200, sortedLocalities, FetchedSuccessfully);
+};
 
 export const getLocality = async (name: string, region: LocalityRegionEnum) => {
   return await localitiesStore.find((locality) => {
-    return locality.name === name && locality[region]
-  })
-}
+    return locality.name === name && locality[region];
+  });
+};
